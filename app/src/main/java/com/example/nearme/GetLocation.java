49dc64e6c56ba55c8894559fc34b5e4049d1b079
Marc Mapeke <mmapeke@fb.com>
2020-07-21 17:42:40 -0400

    user can enter location using autocomplete

--- app/src/main/java/com/example/nearme/GetLocation.java
+++ app/src/main/java/com/example/nearme/GetLocation.java
@@ -1,6 +1,7 @@
 package com.example.nearme;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -14,8 +15,11 @@ import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -26,10 +30,23 @@ import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
@@ -44,16 +61,33 @@ import static com.google.android.gms.location.LocationServices.getFusedLocationP
 public class GetLocation extends AppCompatActivity {
 
     public static final String TAG = "GetLocation";
+    public static final int AUTOCOMPLETE_REQUEST_CODE = 100;
 
-    private LocationRequest mLocationRequest;
     private Button btnCurrentLocation;
+    private Button btnChooseLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_location);
 
+        //Init Places
+        Places.initialize(getApplicationContext(),getResources().getString(R.string.google_maps_api_key));
+
         btnCurrentLocation = findViewById(R.id.btnCurrentLocation);
+        btnChooseLocation = findViewById(R.id.btnChooseLocation);
+
+        btnChooseLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Initialize place field list
+                List<Place.Field> fieldList =  Arrays.asList(Place.Field.NAME, Place.Field.LAT_LNG);
+
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN,fieldList)
+                        .build(GetLocation.this);
+                startActivityForResult(intent,AUTOCOMPLETE_REQUEST_CODE);
+            }
+        });
 
         //TODO: Loading button
         btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +97,20 @@ public class GetLocation extends AppCompatActivity {
                 GetLocationPermissionsDispatcher.getLastLocationWithPermissionCheck(GetLocation.this);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AUTOCOMPLETE_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i(TAG,"Place received: " + place.getName());
+                onLocation(place.getLatLng());
+            }else{
+                Log.e(TAG,"error on activity result");
+            }
+        }
     }
 
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
@@ -77,7 +125,8 @@ public class GetLocation extends AppCompatActivity {
                         // GPS location can be null if GPS is switched off
                         if (location != null) {
                             Log.i(TAG,"location found " + location.toString());
-                            onLocation(location);
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            onLocation(latLng);
                         }else{
                             Log.e(TAG,"location null");
                         }
@@ -91,25 +140,29 @@ public class GetLocation extends AppCompatActivity {
                 });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: delegate the permission handling to generated method
+        GetLocationPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
+    }
 
-    public void onLocation(Location location) {
-        // New location has now been determined
-        String msg = "Current Location: " +
-                Double.toString(location.getLatitude()) + "," +
-                Double.toString(location.getLongitude());
+    public void onLocation(LatLng inp){
+        String msg = "Location: " +
+                Double.toString(inp.latitude) + "," +
+                Double.toString(inp.longitude);
         Log.i(TAG,msg);
 
         //Grab Current User
         ParseUser parseUser = ParseUser.getCurrentUser();
         //Update GeoPoint
-        ParseGeoPoint parseGeoPoint = new ParseGeoPoint(location.getLatitude(),location.getLongitude());
+        ParseGeoPoint parseGeoPoint = new ParseGeoPoint(inp.latitude,inp.longitude);
         parseUser.put("location",parseGeoPoint);
         parseUser.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if(e == null){
                     Log.i(TAG,"location updated");
-//                    Toast.makeText(GetLocation.this,"Location Updated",Toast.LENGTH_SHORT).show();
                 }else{
                     Log.e(TAG,"location not updated");
                 }
@@ -118,13 +171,6 @@ public class GetLocation extends AppCompatActivity {
 
         //Go to Main Activity
         goMainActivity();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // NOTE: delegate the permission handling to generated method
-        GetLocationPermissionsDispatcher.onRequestPermissionsResult(this,requestCode,grantResults);
     }
 
     private void goMainActivity() {
